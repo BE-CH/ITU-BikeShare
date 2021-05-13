49cfff6a480a51ba4008ae809b05e7c7e948e70b
BE-CH <mikkel.l.bech@gmail.com>
2021-05-13 10:55:21 -0700

    + removed some dots

--- javascript/bikeOverview.js
+++ javascript/bikeOverview.js
@@ -1,21 +1,21 @@
-function loadAllBikes(){
-    Object.keys(allbikes).forEach(key => {
-        console.log(allbikes[key]);
-        let container = document.createElement("div");
-        container.setAttribute("class", "bikeCard");
+function loadAllBikes() {
+  Object.keys(allbikes).forEach((key) => {
+    console.log(allbikes[key]);
+    let container = document.createElement('div');
+    container.setAttribute('class', 'bikeCard');
 
-        let titleCon = document.createElement("h3");
-        let titleTxt = document.createTextNode(allbikes[key].title);
-        titleCon.appendChild(titleTxt);
+    let titleCon = document.createElement('h3');
+    let titleTxt = document.createTextNode(allbikes[key].title);
+    titleCon.appendChild(titleTxt);
 
-        let image = document.createElement("img");
-        image.setAttribute("src", "../images/"+allbikes[key].img);
-        
-        let descriptionCon = document.createElement("p");
-        let descriptionTxt = document.createTextNode(allbikes[key].description);
-        descriptionCon.appendChild(descriptionTxt);
+    let image = document.createElement('img');
+    image.setAttribute('src', 'images/' + allbikes[key].img);
 
-        container.append(titleCon, image, descriptionCon);
-        document.getElementById("bikeOverview").appendChild(container);
-    })
+    let descriptionCon = document.createElement('p');
+    let descriptionTxt = document.createTextNode(allbikes[key].description);
+    descriptionCon.appendChild(descriptionTxt);
+
+    container.append(titleCon, image, descriptionCon);
+    document.getElementById('bikeOverview').appendChild(container);
+  });
 }
