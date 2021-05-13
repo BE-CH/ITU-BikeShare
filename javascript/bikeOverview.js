a6db5b6ccff44f34bc37fa5893769cb725ff2f7d
Laurits Bonde <lauritsbonde@hotmail.dk>
2021-05-13 21:19:22 +0200

    prettier changes

--- javascript/bikeOverview.js
+++ javascript/bikeOverview.js
@@ -1,26 +1,25 @@
-
-function loadAllBikes(){
-    Object.keys(bikeTypes).forEach(key => {
+function loadAllBikes() {
+    Object.keys(bikeTypes).forEach((key) => {
         console.log(bikeTypes[key]);
-        let container = document.createElement("div");
-        container.setAttribute("class", "bikeCard");
+        let container = document.createElement('div');
+        container.setAttribute('class', 'bikeCard');
 
-        let titleCon = document.createElement("h3");
+        let titleCon = document.createElement('h3');
         let titleTxt = document.createTextNode(bikeTypes[key].title);
         titleCon.appendChild(titleTxt);
 
-        let image = document.createElement("img");
-        image.setAttribute("src", "../images/"+bikeTypes[key].img);
-        
-        let descriptionCon = document.createElement("p");
+        let image = document.createElement('img');
+        image.setAttribute('src', '../images/' + bikeTypes[key].img);
+
+        let descriptionCon = document.createElement('p');
         let descriptionTxt = document.createTextNode(bikeTypes[key].description);
         descriptionCon.appendChild(descriptionTxt);
 
-    let descriptionCon = document.createElement('p');
-    let descriptionTxt = document.createTextNode(allbikes[key].description);
-    descriptionCon.appendChild(descriptionTxt);
+        let descriptionCon = document.createElement('p');
+        let descriptionTxt = document.createTextNode(allbikes[key].description);
+        descriptionCon.appendChild(descriptionTxt);
 
-    container.append(titleCon, image, descriptionCon);
-    document.getElementById('bikeOverview').appendChild(container);
-  });
+        container.append(titleCon, image, descriptionCon);
+        document.getElementById('bikeOverview').appendChild(container);
+    });
 }
--- javascript/main.js
+++ javascript/main.js
@@ -1,80 +1,82 @@
 let bikeTypes = {
     citybike: {
-        title: "Citybike",
-        description: "Avenue Broadway er en smart citybike med et gennemført flot design. Cyklen har 7 indvendige Shimano gear og rullebremser,  samt hydroformede aluminiumsrør gør denne citybike perfekt til kørsel på jævnt terran.",
-        img: "citybike.png"
-    }, 
+        title: 'Citybike',
+        description:
+            'Avenue Broadway er en smart citybike med et gennemført flot design. Cyklen har 7 indvendige Shimano gear og rullebremser,  samt hydroformede aluminiumsrør gør denne citybike perfekt til kørsel på jævnt terran.',
+        img: 'citybike.png',
+    },
     mountainbike: {
-        title: "Mountain bike",
-        description: "Superior XP 979 er en hurtig mountainbike perfekt til offroading. Cyklen er udstyret med 1x12 Shimano XT/SLX gear og Shimano XT hydrauliske skivebremser.",
-        img: "mountainbike.png"
+        title: 'Mountain bike',
+        description: 'Superior XP 979 er en hurtig mountainbike perfekt til offroading. Cyklen er udstyret med 1x12 Shimano XT/SLX gear og Shimano XT hydrauliske skivebremser.',
+        img: 'mountainbike.png',
     },
     ladybike: {
-        title: "Damecykel",
-        description: "Kildemoes City 257 er en praktisk damecykel med lav indstigning. Den er bygget på et holdbart og let stel i aluminium. Cyklen er udstyret med 7 indvendige gear, en fodbremse samt en fælgbremse. ",
-        img: "damecykel.png"
+        title: 'Damecykel',
+        description:
+            'Kildemoes City 257 er en praktisk damecykel med lav indstigning. Den er bygget på et holdbart og let stel i aluminium. Cyklen er udstyret med 7 indvendige gear, en fodbremse samt en fælgbremse. ',
+        img: 'damecykel.png',
     },
     ebike: {
-        title: "Elcykel",
-        description: "Riese & Müller superchargeren praler ikke med sine indre værdier, den har dem bare. To integrerede Bosch PowerTube batterier.  Magura MT4 skivebremser, luftaffjedring og store dæk.",
-        img: "elcykel.png"
+        title: 'Elcykel',
+        description:
+            'Riese & Müller superchargeren praler ikke med sine indre værdier, den har dem bare. To integrerede Bosch PowerTube batterier.  Magura MT4 skivebremser, luftaffjedring og store dæk.',
+        img: 'elcykel.png',
     },
     echristania: {
-        title: "El ladcykel",
-        description: "Mustang Family elladcykel har kraftige skivebremser, parkeringsbremse og 4 børneseler  så der er fuld fokus på sikkerheden. Mustang Family er både sikker for dig og din familie. ",
-        img: "elLadcykel.png"
-    }
-}
+        title: 'El ladcykel',
+        description:
+            'Mustang Family elladcykel har kraftige skivebremser, parkeringsbremse og 4 børneseler  så der er fuld fokus på sikkerheden. Mustang Family er både sikker for dig og din familie. ',
+        img: 'elLadcykel.png',
+    },
+};
 
-const apiKey = "AIzaSyCKOqKObTh0OqKcHyFiq19fNiIWW9PyLWg";
+const apiKey = 'AIzaSyCKOqKObTh0OqKcHyFiq19fNiIWW9PyLWg';
 
 let allLocations = [
     {
-        name: "FriBikeShop Østerbro",
+        name: 'FriBikeShop Østerbro',
         availableBikes: 10,
-        coord: {lat: 55.7072734, lng: 12.5755586},
-        address: "Østerbrogade 140, 2100 København",
-        openingHours: "10-17"
-    }, 
+        coord: { lat: 55.7072734, lng: 12.5755586 },
+        address: 'Østerbrogade 140, 2100 København',
+        openingHours: '10-17',
+    },
     {
-        name: "BartHolt Cykler",
+        name: 'BartHolt Cykler',
         availableBikes: 7,
-        coord: {lat: 55.7069002, lng: 12.5755175},
-        address: "Østerbrogade 138, 2100 København",
-        openingHours: "09-17"
+        coord: { lat: 55.7069002, lng: 12.5755175 },
+        address: 'Østerbrogade 138, 2100 København',
+        openingHours: '09-17',
     },
     {
-        name: "Saxil Cykler Østerbro",
+        name: 'Saxil Cykler Østerbro',
         availableBikes: 11,
-        coord: {lat: 55.7056537, lng: 12.5738675},
-        address: "Østerfælled Torv 19, 2100 København",
-        openingHours: "10-22"
+        coord: { lat: 55.7056537, lng: 12.5738675 },
+        address: 'Østerfælled Torv 19, 2100 København',
+        openingHours: '10-22',
     },
     {
-        name: "CykelExperten Vanløse",
+        name: 'CykelExperten Vanløse',
         availableBikes: 10,
-        coord: {lat: 55.6978498, lng: 12.4726715},
-        address: "Slotsherrensvej 103, 2720 Vanløse",
-        openingHours: "08-15"
-    }
-]
+        coord: { lat: 55.6978498, lng: 12.4726715 },
+        address: 'Slotsherrensvej 103, 2720 Vanløse',
+        openingHours: '08-15',
+    },
+];
 
 function initMap() {
     // The location of Uluru
     const uluru = { lat: 55.71, lng: 12.57 };
     // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
+    const map = new google.maps.Map(document.getElementById('map'), {
         zoom: 8,
         center: uluru,
     });
 
     const markers = allLocations.map((location, i) => {
-        console.log(location);
-
         const image = {
-            url: "images/shop.svg",
-            
-            scaledSize: new google.maps.Size(35, 35)
+            url: 'images/shop.svg',
+
+            scaledSize: new google.maps.Size(35, 35),
         };
 
         const marker = new google.maps.Marker({
@@ -83,48 +85,50 @@ function initMap() {
             label: location.name,
             map,
             icon: image,
-            optimized: true
+            optimized: true,
         });
 
-        marker.addListener("click", () => {
+        marker.addListener('click', () => {
             openShop(i);
         });
         return marker;
     });
 
     new MarkerClusterer(map, markers, {
-        imagePath:
-        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
     });
 }
 
-
 let shopIsOpen = false;
-function openShop(location){
-    let shopInfo = document.getElementById("shopInfo");
-    if(shopIsOpen){
-        shopInfo.innerHTML = "";
+function openShop(location) {
+    let shopInfo = document.getElementById('shopInfo');
+    if (shopIsOpen) {
+        shopInfo.innerHTML = '';
     }
 
     let shop = allLocations[location];
 
-    let nameHeader = document.createElement("h5");
+    let nameHeader = document.createElement('h5');
     let nameTxt = document.createTextNode(shop.name);
     nameHeader.appendChild(nameTxt);
 
-    let addressField = document.createElement("p");
+    let addressField = document.createElement('p');
     let addressTxt = document.createTextNode(shop.address);
     addressField.appendChild(addressTxt);
 
-    let openingHoursField = document.createElement("p");
-    let openingHours = document.createTextNode("Åbent hver dag: " +shop.openingHours);
+    let openingHoursField = document.createElement('p');
+    let openingHours = document.createTextNode('Åbent hver dag: ' + shop.openingHours);
     openingHoursField.appendChild(openingHours);
 
-    let bikesStored = document.createElement("p");
-    let bikes = document.createTextNode("Ledige cykler: " +shop.availableBikes);
+    let bikesStored = document.createElement('p');
+    let bikes = document.createTextNode('Ledige cykler: ' + shop.availableBikes);
     bikesStored.appendChild(bikes);
 
-    shopInfo.append(nameHeader, addressField, openingHours, bikesStored);
+    let button = document.createElement('button');
+    let butTxt = document.createTextNode('Gå til butik');
+    button.appendChild(butTxt);
+
+    shopInfo.append(nameHeader, addressField, openingHours, bikesStored, button);
 
     shopIsOpen = true;
 }
