d8fa28c86e1c1671e4a085750726521b6a121077
BE-CH <mikkel.l.bech@gmail.com>
2021-05-16 21:35:07 +0200

    + locations

--- javascript/locations.js
+++ javascript/locations.js
@@ -154,12 +154,12 @@ function calcNearestLocation(pos) {
 
 function haversine(pos1, pos2) {
     const R = 6371e3; // metres
-    const φ1 = (pos1.lat * Math.PI) / 180; // φ, λ in radians
-    const φ2 = (pos2.lat * Math.PI) / 180;
-    const Δφ = ((pos2.lat - pos1.lat) * Math.PI) / 180;
-    const Δλ = ((pos2.lng - pos1.lng) * Math.PI) / 180;
+    const lat1 = (pos1.lat * Math.PI) / 180;
+    const lat2 = (pos2.lat * Math.PI) / 180;
+    const latDiff = ((pos2.lat - pos1.lat) * Math.PI) / 180;
+    const longDiff = ((pos2.lng - pos1.lng) * Math.PI) / 180;
 
-    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const a = Math.sin(latDiff / 2) * Math.sin(latDiff / 2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(longDiff / 2) * Math.sin(longDiff / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
     const d = R * c; // in metres
